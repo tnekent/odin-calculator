9b92795816ac19784ca4f1e66e95d789cf2a7f64
Kent Ahon <46017299+tnekent@users.noreply.github.com>
2024-07-16 20:56:35 +0800

    Add division function

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function multiply(a, b) {
     return a * b;
 }
 
+function divide(a, b) {
+    return a / b;
+}
+
 const calcDisplay = document.querySelector("#calc-display");
 const numberButtons = document.querySelectorAll(".btn-num");
 numberButtons.forEach(btn => {
